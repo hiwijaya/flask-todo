f6b48fbfa4fde64e2ce6e5993a47401928ea172f
Happy Indra Wijaya <eng.hiwijaya@gmail.com>
2019-06-23 10:36:58 +0700

    update wrong templates location

--- app/view.py
+++ app/view.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 index_blueprint = Blueprint('index', __name__)
 
 @index_blueprint.route('/', methods=['GET'])
 def index():
-    return 'FLASK-TODO'
+    return render_template('index.html')
