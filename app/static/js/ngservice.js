5df1e2f701fe5f415e0bfe8221901672c155eefd
Happy Indra Wijaya <happyindrawijaya@happy-mcpro.local>
2019-06-26 17:03:37 +0700

    add angularjs

--- app/static/js/ngservice.js
+++ app/static/js/ngservice.js
@@ -1,1 +1,35 @@
 'use strict';
+
+var app = angular.module('app', []);
+
+app.controller('TaskCtrl',
+    function($http, $scope){
+
+        $scope.init = function() {
+
+            $http({
+                method: 'GET',
+                url: '/api/get-tasks',
+                data: null
+            }).success(function(data){
+                alert('HELOOOOOO');
+                $scope.tasks = data.data;
+                console.log(data.data);
+
+
+            }).error(function(data, status, headers, config){
+
+            });
+
+            $http({
+                method: 'GET',
+                url: '/api/get-tasks',
+                data: null
+            })
+
+
+        }
+
+
+
+    });
