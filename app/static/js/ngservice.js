37c0e820ac70c0f34514accf145022b7c9c2cd82
Happy Indra Wijaya <eng.hiwijaya@gmail.com>
2019-06-26 22:37:18 +0700

    add more event

--- app/static/js/ngservice.js
+++ app/static/js/ngservice.js
@@ -3,31 +3,54 @@
 var app = angular.module('app', []);
 
 app.controller('TaskCtrl',
-    function($http, $scope){
+    function ($http, $scope) {
 
-        $scope.init = function() {
+        var taskList = document.getElementById('task-list');
+        var boxInput = document.getElementById('box-input');
+        var boxEmpty = document.getElementById('box-empty');
+        var txtTask = document.getElementById('txt-task');
+        var txtDetail = document.getElementById('txt-detail');
+        var btnDetail = document.getElementById('btn-detail');
+        var btnCancel = document.getElementById('btn-cancel');
+        var btnSave = document.getElementById('btn-save');
+        var btnAdd = document.getElementById('btn-add');
 
+
+
+        $scope.init = function () {
+            boxInput.style.display = 'none';
+            txtDetail.style.display = 'none';
+            
             $http({
                 method: 'GET',
                 url: '/api/get-tasks',
                 data: null
-            }).success(function(data){
-                alert('HELOOOOOO');
-                $scope.tasks = data.data;
-                console.log(data.data);
+            }).then(function success(response) {
 
+                var data = response.data.data;
+                var status = response.data.status;
+                $scope.tasks = data;
 
-            }).error(function(data, status, headers, config){
-
+            }, function fail(response) {
+                console.log(response);
             });
+        }
 
-            $http({
-                method: 'GET',
-                url: '/api/get-tasks',
-                data: null
-            })
+        $scope.newTask = function() {
+             boxInput.style.display = 'block';
+             btnAdd.style.display = 'none';
+        }
 
+        btnDetail.onclick = function () {
+            txtDetail.style.display = 'block';
+        }
 
+        $scope.reset = function() {
+            boxInput.style.display = 'none';
+            btnAdd.style.display = 'block';
+
+            txtTask.value = '';
+            txtDetail.value = '';
         }
 
 
